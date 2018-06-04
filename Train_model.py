7dbc853e7298512402e47aeef2307716257e0796
Jcoetzee609 <jcoetzee609@gmail.com>
2018-06-04 21:33:32 +0200

    V0.01
    Starting with pre processing data

--- Train_model.py
+++ Train_model.py
@@ -1,7 +1,16 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sb
+import seaborn as sns
 
 test = pd.read_csv('test.csv')
-print(test.head())
+train = pd.read_csv('train.csv')
+
+print(train.describe())
+df =train.groupby("Embarked").mean()
+print(df)
+
+grid = sns.FacetGrid(train, row='Embarked', size=2.2, aspect=1.6)
+grid.map(sns.pointplot, 'Pclass', 'Survived', 'Sex', palette='deep')
+grid.add_legend()
+plt.show()
